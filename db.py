66c75284f850020807e67822b9008da73c40f539
Vlad <vlad.lisovskyi@gmail.com>
2019-12-17 21:01:40 +0200

    Added client side, view. Small fixes.

--- db.py
+++ db.py
@@ -1,3 +1,32 @@
+import click
 from flask_sqlalchemy import SQLAlchemy
+from flask import current_app, g
+from flask.cli import with_appcontext
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
+
+
+def init_db():
+    from .models import User, Record, Item
+    db.drop_all()
+    db.create_all()
+    db.session.execute("""DROP VIEW IF EXISTS records_view;""")
+    db.session.execute("""
+    CREATE VIEW records_view AS SELECT records.id, records.operation_time, users.name AS user_name, records.operation, items.name AS item_name,
+     records.quantity FROM records INNER JOIN users ON records.user_id=users.id INNER JOIN items ON records.item_id=items.id;
+    """)
+    admin = User(name="admin", phone="0965657867", password=generate_password_hash("1234qwer"), valid=1, admin=1)
+    db.session.add(admin)
+    db.session.commit()
+
+
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo("Initialized the database.")
+
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
--- view.py
+++ view.py
@@ -1,13 +1,13 @@
 from flask import (
-    Blueprint, redirect, render_template, request, url_for, flash
+    Blueprint, redirect, render_template, request, url_for, flash, g
 )
-
 from werkzeug.exceptions import abort
 from werkzeug.security import generate_password_hash
 from .auth import login_required
 from .db import db
 from .models import User, Item, Record
 from sqlalchemy.exc import StatementError
+
 
 bp = Blueprint('view', __name__)
 
@@ -22,7 +22,6 @@ names = db.metadata.tables.keys()
 @bp.route('/')
 @login_required
 def index():
-    print(names)
     return render_template('view/index.html', names=names)
 
 
@@ -30,6 +29,8 @@ def index():
 @login_required
 def viewer(tname):
     cur = db.session.execute(f"SELECT * FROM {tname}")
+    if tname == "records":
+        cur = db.session.execute(f"SELECT * FROM records_view")
     results = [{column: value for column, value in row.items()} for row in cur]
     col_names = [elem for elem in cur.keys()]
     return render_template('view/view.html', tname=tname, col_names=col_names, results=results, names=names)
@@ -59,10 +60,11 @@ def inserter(tname):
                 new = tables[tname](**k)
                 db.session.add(new)
                 db.session.commit()
+                flash("Data inserted", "success")
             else:
                 abort(500)
         except StatementError:
-            flash("Got wrong value. Please write for Boolean 1/0 or True/False.")
+            flash("Got wrong value. Please write for Boolean 1/0 or True/False.", "error")
 
         return redirect(url_for('view.viewer', tname=tname))
 
@@ -87,7 +89,7 @@ def editor(tname, id):
         elif 'password' in k.keys():
             k.pop('password')
 
-        if "valid" in k.keys() and k['valid'] in boolean.keys():
+        if "valid" in k.keys() and k['valid'].lower() in boolean.keys():
             k["valid"] = boolean[k['valid'].lower()]
         if "admin" in k.keys() and k['admin'] in boolean.keys():
             k["admin"] = boolean[k['admin'].lower()]
@@ -98,10 +100,11 @@ def editor(tname, id):
                 for key, value in k.items():
                     setattr(new, key, value)
                     db.session.commit()
+                flash("Data changed", "success")
             else:
                 abort(500)
-        except StatementError:
-            flash("Got wrong value. Please write for Boolean 1/0 or True/False.")
+        except StatementError as e:
+            flash("Got wrong value. Please write for Boolean 1/0 or True/False.", "error")
 
         return redirect(url_for('view.viewer', tname=tname))
 
@@ -114,10 +117,43 @@ def form_client():
     if request.method == "POST":
         k = request.form.to_dict()
         error = None
-        print(k)
         if k["option"] == 'addOption':
-            pass
+            if k['itemControl'] == 'addNew' and k['newItem'] != "":
+                new = Item(name=k["newItem"], quantity=k['quantityField'])
+                db.session.add(new)
+                db.session.commit()
+                obj = Item.query.filter_by(name=new.name).first()
+                new_rec = Record(user_id=g.user.id, operation="ADDED", item_id=obj.id, quantity=k["quantityField"])
+                db.session.add(new_rec)
+                db.session.commit()
+                flash(f"{new.name} added!", "success")
+                return render_template('view/index.html', names=names)
+            elif k['itemControl'] in [str(i[0]) for i in db.session.query(Item.id).all()]:
+                obj = Item.query.get(int(k['itemControl']))
+                obj.quantity = int(obj.quantity) + int(k['quantityField'])
+                new_rec = Record(user_id=g.user.id, operation="ADDED", item_id=obj.id, quantity=k["quantityField"])
+                db.session.add(new_rec)
+                db.session.commit()
+                flash(f"Successfully added {k['quantityField']} to {obj.name}!", "success")
+                return render_template('view/index.html', names=names)
+            else:
+                error = "Problems with entered data."
+        elif k["option"] == "takeOption":
+            obj = Item.query.get(int(k['itemControl']))
+            if int(k["quantityField"]) <= int(obj.quantity):
+                obj.quantity = int(obj.quantity) - int(k['quantityField'])
+                new_rec = Record(user_id=g.user.id, operation="TOOK", item_id=obj.id, quantity=k["quantityField"])
+                db.session.add(new_rec)
+                db.session.commit()
+                flash(f"Successfully took  {k['quantityField']} from {obj.name}!", "success")
+                return render_template('view/index.html', names=names)
+            else:
+                error = f"You trying to take {k['quantityField']} from {obj.name} but there is only {obj.quantity}!"
+        else:
+            error = "Troubles occurred."
 
+        if error is None:
+            return render_template('view/index.html', names=names)
 
-
-    return render_template("view/client_form.html", items=items)
+        flash(error, "error")
+    return render_template("view/client_form.html", items=items, names=names)
